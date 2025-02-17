f243cf053af0fc2ebbf469865bc60d58fc473f4a
therickybobbeh <rcole2597@gmail.com>
2025-02-17 07:53:22 -0500

    second

--- src/app/components/med-table/med-table.component.ts
+++ src/app/components/med-table/med-table.component.ts
@@ -2,37 +2,30 @@ import { Component, Input, OnInit } from '@angular/core';
 import {FormArray, FormGroup, ReactiveFormsModule} from '@angular/forms';
 import { MedicineFormGroup } from '../../models/medicine';
 import {TableModule} from 'primeng/table';
+import {NgClass, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-med-table',
   templateUrl: './med-table.component.html',
   imports: [
+    ReactiveFormsModule,
     TableModule,
-    ReactiveFormsModule
+    NgClass,
+    NgIf
   ],
   styleUrls: ['./med-table.component.css']
 })
 export class MedTableComponent implements OnInit {
   @Input() formArray!: FormArray;
+  expandedRows: { [key: number]: boolean } = {};
 
   getBrandsArray(medicineFormGroup: FormGroup): FormArray {
     return medicineFormGroup.get('brands') as FormArray;
   }
 
+  toggleRow(rowIndex: number): void {
+    this.expandedRows[rowIndex] = !this.expandedRows[rowIndex];
+  }
+
   ngOnInit(): void {}
 }
-
-
-
-  // get nameControl(): FormControl {
-  //   return this.form.get('name') as FormControl;
-  // }
-  //
-  // get dosageControl(): FormControl {
-  //   return this.form.get('dosage') as FormControl;
-  // }
-  //
-  // get frequencyControl(): FormControl {
-  //   return this.form.get('frequency') as FormControl;
-  // }
-
